917c54ec8b2d6ddd36037f6b763fef65fa4cd244
yawarpwn <john.kazz@gmail.com>
2023-10-28 17:14:41 -0500

    chapter 10 search and pagination finished

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -26,13 +26,13 @@ export default async function InvoicesTable({
                 <div className="flex items-center justify-between border-b pb-4">
                   <div>
                     <div className="mb-2 flex items-center">
-                      <Image
-                        src={invoice.image_url}
-                        className="mr-2 rounded-full"
-                        width={28}
-                        height={28}
-                        alt={`${invoice.name}'s profile picture`}
-                      />
+                      {/* <Image */}
+                      {/*   src={invoice.image_url} */}
+                      {/*   className="mr-2 rounded-full" */}
+                      {/*   width={28} */}
+                      {/*   height={28} */}
+                      {/*   alt={`${invoice.name}'s profile picture`} */}
+                      {/* /> */}
                       <p>{invoice.name}</p>
                     </div>
                     <p className="text-sm text-gray-500">{invoice.email}</p>
@@ -85,14 +85,14 @@ export default async function InvoicesTable({
                 >
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex items-center gap-3">
-                      <Image
-                        src={invoice.image_url}
-                        className="rounded-full"
-                        width={28}
-                        height={28}
-                        alt={`${invoice.name}'s profile picture`}
-                      />
-                      <p>{invoice.name}</p>
+                      {/* <Image */}
+                      {/*   src={invoice.image_url} */}
+                      {/*   className="rounded-full" */}
+                      {/*   width={28} */}
+                      {/*   height={28} */}
+                      {/*   alt={`${invoice.name}'s profile picture`} */}
+                      {/* /> */}
+                      <p>ra{invoice.name}zi</p>
                     </div>
                   </td>
                   <td className="whitespace-nowrap px-3 py-3">
